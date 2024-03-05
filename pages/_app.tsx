b5381adaf864539d0e68424d6dbcfd4bdf88a464
Yin-Lynn-Htun <yinlynnhtun@rezeve.com>
2024-03-05 22:50:36 +0700

    Chore: Setup and install packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  preload: true,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />{' '}
+    </main>
+  )
 }
