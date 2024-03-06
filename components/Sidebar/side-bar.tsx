1419cd8fb73f65c58e74461a33a3968ca6532c99
Kyaw Oo <kyawkyawjek@gmail.com>
2024-03-06 22:17:14 +0700

    Added Sidebar component

---
+++ components/Sidebar/side-bar.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { ChevronFirst, LogOut, Menu, User, User2 } from "lucide-react";
+import { MenuLinks } from "@/constants/links";
+import PlaceHolderImg from "@/public/placeholder-img.png";
+import Image from "next/image";
+import MenuLink from "./menu-link";
+
+function getLocalIsSidebarOpen() {
+  return localStorage.getItem("isSidebarOpen") === "true" ? true : false;
+}
+
+const FULL_WIDTH = 72;
+const COLLAPSED_WIDTH = 20;
+
+export default function Sidebar({ children }: { children: React.ReactNode }) {
+  const [mount, setMount] = useState(false);
+  const pathName = usePathname();
+  const [open, setOpen] = useState(
+    typeof window !== "undefined" ? getLocalIsSidebarOpen : null
+  );
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  return (
+    <>
+      {mount && (
+        <div className='flex'>
+          <div
+            className={`${
+              open ? `w-${FULL_WIDTH}` : `w-${COLLAPSED_WIDTH}`
+            } bg-primary h-screen p-5 pt-7 duration-300 fixed flex flex-col justify-between`}
+          >
+            <div
+              className={`absolute cursor-pointer -right-3 top-9 w-6 h-6 grid place-items-center ring-2 ring-background bg-accent rounded-full  ${
+                !open && "rotate-180"
+              }`}
+              onClick={() => {
+                localStorage.setItem("isSidebarOpen", `${!open}`);
+                setOpen(getLocalIsSidebarOpen);
+              }}
+            >
+              <ChevronFirst size={18} color='white' />
+            </div>
+
+            <div>
+              <div className='flex gap-x-4 items-center'>
+                <Image
+                  src={PlaceHolderImg}
+                  className={`rounded-full ${!open && ""}`}
+                  width={40}
+                  height={40}
+                  alt='logo'
+                />
+                <h1
+                  className={`text-white origin-left font-medium text-md duration-300 whitespace-nowrap ${
+                    !open && "hidden"
+                  }`}
+                >
+                  Wyne School Portal
+                </h1>
+              </div>
+              <ul className='pt-6'>
+                <div>
+                  {MenuLinks.map((Menu) => (
+                    <MenuLink
+                      icon={{
+                        src: Menu.src,
+                      }}
+                      path={Menu.path}
+                      title={Menu.title}
+                      gap={Menu.gap}
+                      key={Menu.path}
+                      pathName={pathName}
+                      open={open}
+                    />
+                  ))}
+                </div>
+              </ul>
+            </div>
+            <div>
+              <hr className='mt-9 mb-6' />
+              <MenuLink
+                icon={{
+                  src: User,
+                  img: PlaceHolderImg.src,
+                }}
+                path={"/profile"}
+                pathName={pathName}
+                title={"User Name"}
+                open={open}
+              />
+              <MenuLink
+                icon={{
+                  src: LogOut,
+                }}
+                pathName={pathName}
+                title={"Logout"}
+                open={open}
+              />
+            </div>
+          </div>
+
+          <main
+            className={`flex-1 p-5 pt-3 duration-300 ${
+              open ? `ml-${FULL_WIDTH}` : `ml-${COLLAPSED_WIDTH}`
+            }`}
+          >
+            {children}
+          </main>
+        </div>
+      )}
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Poppins } from 'next/font/google'
+import Sidebar from "@/components/Sidebar/side-bar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   preload: true,
-})
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
-      <Component {...pageProps} />{' '}
+      <Sidebar>
+        <Component {...pageProps} />{" "}
+      </Sidebar>
     </main>
-  )
+  );
 }
