ae766962ef6f778b6f015ec3d358ea4cd051738a
Kyaw Oo <kyawkyawjek@gmail.com>
2024-03-31 11:26:11 +0700

    Refactor announcement creation logic and update department name display

--- pages/announcement/create.tsx
+++ pages/announcement/create.tsx
@@ -30,6 +30,12 @@ export default function AccouncementCreate() {
     e.preventDefault()
     console.log('payload:', { subject, type, selectedStaff, content })
     setIsLoading(true)
+    let staffIds = selectedStaff.map(staff => staff.id)
+
+    if (type === 'ALL' && staffs) {
+      staffIds = staffs.data.staffs.map(staff => staff.id)
+    }
+
     const res = await mutateAsync({
       url: `announcements/`,
       method: 'POST',
@@ -38,7 +44,7 @@ export default function AccouncementCreate() {
         subject,
         content,
         type,
-        staffIds: selectedStaff.map(staff => staff.id),
+        staffIds,
       },
       invalidateUrls: [`announcements`],
     })
@@ -66,13 +72,13 @@ export default function AccouncementCreate() {
             <div className='flex items-center space-x-2'>
               <RadioGroupItem value='ALL' id='ALL' />
               <Label className='cursor-pointer' htmlFor='ALL'>
-                All staff from {auth?.staff.departmentId} department
+                All staff from <b>{auth?.staff.department.name}</b> department
               </Label>
             </div>
             <div className='flex items-center space-x-2 mt-2'>
               <RadioGroupItem value='SPECIFIC' id='SPECIFIC' />
               <Label className='cursor-pointer' htmlFor='SPECIFIC'>
-                Specific staff from {auth?.staff.departmentId} department
+                Specific staff from <b>{auth?.staff.department.name}</b> department
               </Label>
             </div>
           </RadioGroup>
